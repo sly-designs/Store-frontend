af54f5e0a4faf40821b468c554b90d52807fcc40
slydesigns <sylvestorsielei@gmail.com>
2023-01-18 20:20:48 +0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-
-  `,
-  styles: []
+  template: `./app.component.html `,
 })
 export class AppComponent {
   title = 'store';
