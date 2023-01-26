11abbebac3d26c10d42134a7733cde8c052f7b25
slydesigns <sylvestorsielei@gmail.com>
2023-01-26 21:52:57 +0300

    new categories display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
   cols = 3;
+  category: string | undefined;
 
   constructor() {}
 
@@ -16,6 +17,10 @@ export class HomeComponent implements OnInit{
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
