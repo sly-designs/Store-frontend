0df3a427c608f672347293a0ddb7de6e7a46d4e2
slydesigns <sylvestorsielei@gmail.com>
2023-01-27 22:04:52 +0300

    product box component

---
+++ src/app/pages/home/product-box/product-box.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  template: `
+    <p>
+      product-box works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProductBoxComponent {
+
+}
