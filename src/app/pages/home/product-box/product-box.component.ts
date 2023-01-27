adf19595f9505527a8ce8fc8f5aa9edd3490a6a7
slydesigns <sylvestorsielei@gmail.com>
2023-01-27 22:32:59 +0300

    full width mode

--- src/app/pages/home/product-box/product-box.component.ts
+++ src/app/pages/home/product-box/product-box.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html',
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit{
+ 
+ @Input() fullWidthMode = false;
+  constructor() {}
 
+  ngOnInit(): void {
+    
+  }
 }
