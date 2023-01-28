c24fbbd8d21c9c2174d14bab4d3bdddca8d320c8
slydesigns <sylvestorsielei@gmail.com>
2023-01-28 22:05:19 +0300

    models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { ProductBoxComponent } from './pages/home/product-box/product-box.compon
     HomeComponent,
     ProductsHeaderComponent,
     FiltersComponent,
-    ProductBoxComponent
+    ProductBoxComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
