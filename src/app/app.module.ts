d32e636b518afe2b6268a77691efc5f85c307e7d
slydesigns <sylvestorsielei@gmail.com>
2023-01-28 21:30:55 +0300

    few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/product-box/product-box.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { FiltersComponent } from './pages/components/filters/filters.component';
     HeaderComponent,
     HomeComponent,
     ProductsHeaderComponent,
-    FiltersComponent
+    FiltersComponent,
+    ProductBoxComponent
   ],
   imports: [
     BrowserModule,
