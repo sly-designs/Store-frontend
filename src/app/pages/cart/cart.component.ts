4fdce52eb77f7feade66f36720e15aac1d24c4cb
slydesigns <sylvestorsielei@gmail.com>
2023-01-28 21:55:30 +0300

    models

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+
+  cart: Cart = {items: []};
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
