d22d5dbcfc5fad05676d805771025bb5b66ad845
slydesigns <sylvestorsielei@gmail.com>
2023-01-31 21:32:06 +0300

    profile model details

--- src/app/pages/home/product-box/product-box.component.ts
+++ src/app/pages/home/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,7 +8,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductBoxComponent implements OnInit{
  
  @Input() fullWidthMode = false;
-  product: Product
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snickers',
+    price: 150,
+    category: 'shoes',
+    description: 'New arrivals',
+    image: 'https://via.placeholder.com/150' 
+  };
 
   constructor() {}
 
