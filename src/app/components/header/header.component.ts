b6267221c8699ff211ec2d5ca17e3608db55d2a0
slydesigns <sylvestorsielei@gmail.com>
2023-02-01 21:05:13 +0300

    Item quantity

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Cart } from 'src/app/models/cart.model';
 
 @Component({
@@ -8,6 +8,20 @@ import { Cart } from 'src/app/models/cart.model';
 export class HeaderComponent {
   private _cart: Cart = { items: [] };
   itemsQuantity = 0;
+
+  @Input()
+  get cart (): Cart {
+    return this._cart;
+  }
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+    .map((item) => item.quantity)
+    .reduce((prev, current) => prev + current, 0);
+  }
+
   constructor() {}
 
 }
