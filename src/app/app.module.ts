959b7a64fd3d69dc25b85e4489dae6ae0af82150
slydesigns <sylvestorsielei@gmail.com>
2023-02-01 20:17:29 +0300

    service items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ProductsHeaderComponent } from './pages/components/products-header/prod
 import { FiltersComponent } from './pages/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
 
 
 @NgModule({
@@ -52,7 +53,7 @@ import { CartComponent } from './pages/cart/cart.component';
     MatBadgeModule,
     MatSnackBarModule,
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { Cart, CartItem } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart = new BehaviorSubject<Cart>({ items: []});
 
-  constructor() { }
+  constructor(private_snackBar: MatSnackBar) { }
+
+  addToCart(item: CartItem): void {
+    const items = [...this.cart.value.items];
+  }
+
 }
