882f11653099f74456257a5acf53a158cdaea39c
slydesigns <sylvestorsielei@gmail.com>
2023-02-01 20:24:33 +0300

    adding quantity in cart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -13,6 +13,17 @@ export class CartService {
 
   addToCart(item: CartItem): void {
     const items = [...this.cart.value.items];
+
+    const itemInCart = items.find((_item) => _item.id === item.id);
+
+    if (itemInCart) {
+      itemInCart.quantity += 1;
+    } else {
+      items.push(item);
+    }
+
+    this.cart.next({ items });
+    this._snackBar.open('1 item added to cart.', 'ok', {duration: 3000});
   }
 
 }
