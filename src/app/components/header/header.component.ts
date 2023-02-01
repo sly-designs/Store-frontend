d02ba17a55523c69a144084ee3ae44d4b89c562e
slydesigns <sylvestorsielei@gmail.com>
2023-02-01 21:00:06 +0300

    item quantity

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit{
-
+export class HeaderComponent {
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
   constructor() {}
-
-  ngOnInit(): void {
-    
-  }
 
 }
