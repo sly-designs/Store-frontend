d471d6a298a775fb390ffa5b1d97f34126a1ac38
slydesigns <sylvestorsielei@gmail.com>
2023-02-08 20:59:22 +0300

    public changes

--- server/server.js
+++ server/server.js
@@ -3,3 +3,7 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, Credentials: true }));
