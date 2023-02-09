18d901e682f40309a2ec953bac7624afcbec9fff
slydesigns <sylvestorsielei@gmail.com>
2023-02-09 19:29:21 +0300

    secret key

--- server/server.js
+++ server/server.js
@@ -7,3 +7,5 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cors({ origin: true, Credentials: true }));
+
+const stripes = require("stripe")("sk_test_51MYutsLjlbwfYKGxWN5mcukXLVt12jNqF8BJMcbI4KDlNuNiJumZSShe8B2Vld4VVtQ8cw5fN7dl1r5x46avqYUE00Eyb9ECXv");
