d4bbbc71f1f1e2728ea71ebf30ca276842bd2c25
slydesigns <sylvestorsielei@gmail.com>
2023-02-09 19:36:08 +0300

    try catch

--- server/server.js
+++ server/server.js
@@ -9,3 +9,13 @@ app.use(bodyparser.json());
 app.use(cors({ origin: true, Credentials: true }));
 
 const stripes = require("stripe")("sk_test_51MYutsLjlbwfYKGxWN5mcukXLVt12jNqF8BJMcbI4KDlNuNiJumZSShe8B2Vld4VVtQ8cw5fN7dl1r5x46avqYUE00Eyb9ECXv");
+
+app.post("/checkout", async( req, res, next) => {
+    try {
+        const session = await stripes.checkout.sessions.create({
+
+        });
+    }catch (error) {
+
+    }
+});
